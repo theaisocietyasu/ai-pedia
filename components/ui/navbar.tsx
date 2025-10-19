fc0af26ddac60fce8d1b8029d50a4de11684a15c
ashworks1706 <asriv132@asu.edu>
2025-10-18 17:37:47 -0700

    ui ux

--- components/ui/navbar.tsx
+++ components/ui/navbar.tsx
@@ -103,8 +103,6 @@ export function Navbar() {
   return (
     <>
       <motion.header
-        initial={{ y: -100 }}
-        animate={{ y: 0 }}
         transition={{ type: "spring", stiffness: 100, damping: 20 }}
         className={cn(
           "fixed top-0 left-0 right-0 z-50 transition-all duration-300",
